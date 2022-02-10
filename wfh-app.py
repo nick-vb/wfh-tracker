a74a6506cacd9e7952797227ad82704ebb39e765
Nick vB <63462591+nick-vb@users.noreply.github.com>
2022-02-10 18:55:01 +1000

    Create wfh-app.py

---
+++ wfh-app.py
@@ -0,0 +1,4 @@
+weekNo = int(input("Please enter the week number and press enter: "))
+employID = int(input("Please enter your employee ID and press enter: "))
+employName = str(input("Please enter your full name and press enter: "))
+hours = range(input("Please enter the hours worked for each day this week, separated by commas, then press enter: "))
